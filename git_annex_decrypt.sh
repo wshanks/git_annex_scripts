9fae68163164abe8a143a2a104caae82057c457a
Will S <wsha.code@gmail.com>
2016-06-08 03:34:02 -0400

    Add license text

--- git_annex_decrypt.sh
+++ git_annex_decrypt.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+#  This file is part of willsALMANJ's git annex scripts.
+#
+# willsALMANJ's git annex scripts are free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# willsALMANJ's git annex scripts are distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright 2016, willsALMANJ
+
 usage() {
 	echo "Usage: ga_decrypt.sh -r REMOTE [-k SYMLINK] [-d FILE]"
 	echo ""
